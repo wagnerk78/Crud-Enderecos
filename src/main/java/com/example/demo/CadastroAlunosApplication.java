c44758408535cb84e58ecaf41f5bc40047a8e2e1
Wagner Kroiss <110502518+wagnerk78@users.noreply.github.com>
2023-08-14 11:37:29 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/CadastroAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAlunosApplication.class, args);
+	}
+
+}
